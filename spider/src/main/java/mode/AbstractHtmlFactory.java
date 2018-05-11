b7005353ef13421f6e0dc1b0bdb2ed5a1cfdb961
zhiqiu <zhour@dtdream.com>
2018-05-11 17:26:40 +0800

    matcher

---
+++ spider/src/main/java/mode/AbstractHtmlFactory.java
@@ -0,0 +1,11 @@
+package mode;
+
+import java.util.List;
+
+public abstract class AbstractHtmlFactory {
+
+    public String extractBody(String html){
+        String body = null;
+        return body;
+    }
+}
---
+++ spider/src/main/java/mode/MatchUtil.java
@@ -0,0 +1,27 @@
+package mode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchUtil {
+
+    private static final String HTML_REGEX = ".+\\.(html|htm|com)";
+    private static final String BODY_REGEX = "<body>.+</body>";
+    private static final String IMG_REGEX = ".+\\.(jpg|jpeg|gif|png)";
+
+    private static Pattern HTML_PATTERN = Pattern.compile(HTML_REGEX);
+    private static Pattern BODY_PATTERN = Pattern.compile(BODY_REGEX);
+    private static Pattern IMG_PATTERN = Pattern.compile(IMG_REGEX);
+
+    public static Matcher htmlMatcher(String input){
+        return HTML_PATTERN.matcher(input);
+    }
+
+    public static Matcher bodyMatcher(String input){
+        return BODY_PATTERN.matcher(input);
+    }
+    public static Matcher imgMatcher(String input){
+        return IMG_PATTERN.matcher(input);
+    }
+
+}
