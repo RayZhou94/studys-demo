dbd63d49c58cce90c4cc763ffec8b88a0e32ca24
zhiqiu <zhour@dtdream.com>
2018-05-12 17:59:42 +0800

    matcher

---
+++ spider/src/main/java/io/BufferedWriterDemo.java
@@ -0,0 +1,39 @@
+package io;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterDemo extends FileWriteDemo{
+
+	public BufferedWriterDemo(String fileName, int lineNum) {
+		super(fileName, lineNum);
+	}
+
+	@Override
+	protected void writeInner() {
+		BufferedWriter bw = null;
+		FileWriter fw = null;		
+		
+		try {
+			fw = new FileWriter(this.getFileName());
+			bw = new BufferedWriter(fw);
+			for(int i = 0; i < this.getLineNum(); i++) {
+				bw.write(this.getFileLine());			
+			}
+			bw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+}
